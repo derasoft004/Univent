7db8bc700c960af35c9c8602d02722350f6ff392
Egor <derasoft004@gmail.com>
2024-10-15 18:23:35 +0400

    Seventh commit (egor) - page 'poster.html' and css file 'my_stile.css'  were added before tracking (beta). goood version

--- univent/main_app/views.py
+++ univent/main_app/views.py
@@ -4,4 +4,8 @@ def index(request):
     data = {'var': 15}
     return render(request, 'index.html', context=data)
 
+def poster(request):
+    data: dict = {'posters': dict((f'event_{n}', round(n * 3 + 50 / n)) for n in range(1, 11))}
+    return render(request, 'poster.html', context=data)
+
 
