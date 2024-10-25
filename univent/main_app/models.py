249f4aa52c53c8f4076dd9012db379eddbff2961
Egor <derasoft004@gmail.com>
2024-10-25 13:30:22 +0400

    Ninth commit (egor) - updated Vlad's commit (add registration of bd to admin and PEP8 in models)

--- univent/main_app/models.py
+++ univent/main_app/models.py
@@ -4,11 +4,12 @@ from django.db import models
 class Poster(models.Model):
     title = models.CharField(max_length=255)
     creator = models.CharField(max_length=255)
-    short_description = models.TextField(blank = True, null=True)
-    full_description = models.TextField(blank = True, null=True)
-    time_create = models.DateTimeField(auto_now_add = True)
+    short_description = models.TextField(blank=True, null=True)
+    full_description = models.TextField(blank=True, null=True)
+    time_create = models.DateTimeField(auto_now_add=True)
     time_event = models.DateTimeField()
     preview_image = models.CharField(max_length=255)
 
     def __str__(self):
         return f'{self.creator}_{self.title}'
+
